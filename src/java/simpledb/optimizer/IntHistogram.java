1e8a59da349279b67898d0d85f13fe7c0cc842cd
Erick Meikoki <erickmeikoki@Ericks-iMac.local>
2023-05-19 14:22:40 -0500

    first commit

---
+++ src/java/simpledb/optimizer/IntHistogram.java
@@ -0,0 +1,154 @@
+package simpledb.optimizer;
+
+import simpledb.execution.Predicate;
+
+/**
+ * A class to represent a fixed-width histogram over a single integer-based field.
+ */
+public class IntHistogram {
+
+    /**
+     * Create a new IntHistogram.
+     * <p>
+     * This IntHistogram should maintain a histogram of integer values that it receives.
+     * It should split the histogram into "buckets" buckets.
+     * <p>
+     * The values that are being histogrammed will be provided one-at-a-time through the "addValue()" function.
+     * <p>
+     * Your implementation should use space and have execution time that are both
+     * constant with respect to the number of values being histogrammed.  For example, you shouldn't
+     * simply store every value that you see in a sorted list.
+     *
+     * @param buckets The number of buckets to split the input value into.
+     * @param min     The minimum integer value that will ever be passed to this class for histogramming
+     * @param max     The maximum integer value that will ever be passed to this class for histogramming
+     */
+
+   
+    public int min;
+    private int[] buckets;
+    public int max;
+    private int sizeofBucket;
+    private int numberOfTuples;
+    public int numberOfBuckets;
+
+    public IntHistogram(int buckets, int min, int max) {
+        // TODO: some code goes here
+      
+        this.max=max;
+        this.numberOfBuckets=buckets;
+        this.min=min;
+        this.buckets=new int[numberOfBuckets];
+        this.numberOfTuples=0;
+        this.sizeofBucket=(int) Math.ceil(((double) (this.max - this.min+1))/numberOfBuckets);
+
+    }
+
+    /**
+     * Add a value to the set of values that you are keeping a histogram of.
+     *
+     * @param v Value to add to the histogram
+     */
+    public void addValue(int v) {
+        // TODO: some code goes here
+        int idx = (v-this.min)/this.sizeofBucket;
+        buckets[idx]++;
+        this.numberOfTuples++;
+    }
+
+    /**
+     * Estimate the selectivity of a particular predicate and operand on this table.
+     * <p>
+     * For example, if "op" is "GREATER_THAN" and "v" is 5,
+     * return your estimate of the fraction of elements that are greater than 5.
+     *
+     * @param op Operator
+     * @param v  Value
+     * @return Predicted selectivity of this particular operator and value
+     */
+    public double estimateSelectivity(Predicate.Op op, int v) {
+        // TODO: some code goes here
+        if(v>this.max) {
+            if(op==Predicate.Op.EQUALS || op==Predicate.Op.GREATER_THAN) {
+                return 0;
+            }
+            if(op==Predicate.Op.GREATER_THAN_OR_EQ || op==Predicate.Op.LIKE) {
+                return 0;
+            }
+            return 1;
+        }
+
+        if(v<this.min) {
+            if(op==Predicate.Op.EQUALS || op==Predicate.Op.LESS_THAN)
+                return 0;
+            if(op==Predicate.Op.LESS_THAN_OR_EQ || op== Predicate.Op.LIKE)
+                return 0;
+            return 1;
+        }
+
+        int idx=(v-this.min)/this.sizeofBucket;
+        double w_ =this.sizeofBucket;
+        double h_ =buckets[idx];
+
+    	if (op==Predicate.Op.EQUALS) {
+            return h_/w_/this.numberOfTuples;
+        }
+        else if (op==Predicate.Op.NOT_EQUALS) {
+            return 1-h_/w_/numberOfTuples;
+        }
+
+        
+        int rightBuck= ((int) w_)*(idx+1)+this.min-1;
+        double b__=(rightBuck-v)/w_;
+        double bucket_prev=h_/numberOfTuples;
+        double rrnext=b__*bucket_prev;
+        double resultFrmOtherBuckets=0;
+        for(int k=idx+1; k<numberOfBuckets; k++)
+            resultFrmOtherBuckets=resultFrmOtherBuckets+buckets[k];
+        resultFrmOtherBuckets=resultFrmOtherBuckets/numberOfTuples;
+        double greaterSelec=resultFrmOtherBuckets+rrnext;
+
+        if(op== Predicate.Op.GREATER_THAN)
+            return greaterSelec;
+        else if (op== Predicate.Op.GREATER_THAN_OR_EQ) {
+            return greaterSelec+h_/w_/this.numberOfTuples;
+        }
+        else if (op==Predicate.Op.LESS_THAN_OR_EQ) {
+            return 1-greaterSelec;
+        }
+        else if (op==Predicate.Op.LESS_THAN) {
+            return 1-greaterSelec-h_/w_/this.numberOfTuples;
+        }
+        else if (op== Predicate.Op.LIKE) {
+            return 1/numberOfBuckets;   // ?
+        }
+        return -1.0;
+    }
+
+    /**
+     * @return the average selectivity of this histogram.
+     *         <p>
+     *         This is not an indispensable method to implement the basic
+     *         join optimization. It may be needed if you want to
+     *         implement a more efficient optimization
+     */
+    public double avgSelectivity() {
+        // TODO: some code goes here
+        return 1.0;
+    }
+
+    /**
+     * @return A string describing this histogram, for debugging purposes
+     */
+    public String toString() {
+        // TODO: some code goes here
+        String s = "";
+        int idx = 0;
+
+        while (idx < numberOfBuckets){
+            s = s + Integer.toString(idx) + " = " + Integer.toString(buckets[idx]) + "; ";
+            idx++;
+        }
+        return s; 
+    }
+}
